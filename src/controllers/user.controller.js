e5dafc8c31b2dd00e12dd4c29e3ce800877a5569
sheikh-Zulkifal <sheikhzulkifal1@gmail.com>
2023-11-24 20:54:28 +0500

    add register controller and bug fixes II

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -2,6 +2,8 @@ import {asyncHandler} from "../utils/asyncHandler.js"
 import {ApiError} from "../utils/ApiError.js"
 import {User} from "../models/user.model.js"
 import {uploadOnCloudinary} from "../utils/cloudinary.js"
+import { ApiResponse } from "../utils/ApiResponse.js"
+
 
 const registerUser = asyncHandler ( async (req, res) =>{
   // get user details from frontend
@@ -40,7 +42,29 @@ if (!avtarLocalPath) {
 const avatar = await uploadOnCloudinary(avtarLocalPath)
 const coverImage = await uploadOnCloudinary(coverImageLocalPath)
 
+if (!avatar) {
+    throw new ApiError(400, "Avatar files is required")
+}
 
+const user = await User.create({
+    fullName,
+    avatar: avatar.url,
+    coverImage: coverImage?.url || "",
+    email,
+    username: username.toLowerCase()
+})
+
+const createdUser = await User.findById(user._id).select(
+    "-password -refreshToken" 
+)
+
+if(!createdUser){
+    throw new ApiError(500, "Something went wrong while registering user")
+}
+
+    return res.status(201).json(
+        new ApiResponse(200, createdUser ,"User registered Successfully")
+    )
 })
 
 export {registerUser}
